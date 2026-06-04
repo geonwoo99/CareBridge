9cfe6a3aa0624c1070e73c8f57153b51fecaadff
geonwoo <jungangs0104@gmail.com>
2026-06-04 12:32:00 +0900

    fix: remove obsolete app/admin page causing TypeScript error in production build

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: '/Users/gogeon-u/Carebridge/tina/__generated__/.cache/1780543359673', url: 'http://localhost:4001/graphql', token: '', queries,  });
 export default client;
   
